4b8159bad5b2e8f2ca5dafe7afe60624b8e3bba8
Igor Teles Oliveira <igor.teles@dataprev.gov.br>
2022-08-09 17:38:53 -0300

    Evolucao

---
+++ src/main/java/br/com/alura/loja/modelo/Informatica.java
@@ -0,0 +1,34 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Informatica extends Produto{
+
+	private String marca;
+	private Integer modelo;
+	
+	
+	public Informatica() {
+		
+	}
+	
+	public Informatica(String marca, Integer modelo) {
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setAutor(String marca) {
+		this.marca = marca;
+	}
+	public Integer getModelo() {
+		return modelo;
+	}
+	public void setNumeroDePaginas(Integer modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+}
